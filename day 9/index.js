56f38f68d75855e8e9d28dcc01200c02f82701d9
Ariesh-1974 <arieshgk1974@gmail.com>
2025-01-08 22:20:54 +0530

    update

--- day 9/index.js
+++ day 9/index.js
@@ -5,9 +5,58 @@
 // }
 // console.log("total:",Sum); 
 
-let input1 = prompt("enter1:");
-let input2 = prompt("enter2:");
-console.log(input1.charAt(0).toUpperCase() + input1.slice(1),input2.charAt(0).toUpperCase() + input2.slice(1));
+// let input1 = prompt("enter1:");
+// let input2 = prompt("enter2:");
+// console.log(input1.charAt(0).toUpperCase() + input1.slice(1),input2.charAt(0).toUpperCase() + input2.slice(1));
+//task 1
+function findlargestword(input){
+    let store=input.split(' ');
+    let longestword=0;
+    for(let i=0;i < store.length;i++){
+        if(store[i].length > longestword){
+            longestword=store[i].length;
+        }
+    }
+    console.log(longestword);
+    
+}
+findlargestword('hello ever one');
+//task 2
+function replace(input){
+    input=input.split(' ');
+    for(let i=0;i<input.length;i++){
+        if(input.indexOf(input[i]) != i){
+            input[i]='CHANGED';
+        }
+    }
+    console.log(input.join(' '));
+}
+replace('John Mary John Alex Mary ');
+//task 3
+function replaceeven(words){
+    words=words.split(' ');
+    for(let i=0;i<words.length;i++){
+        if(i%2==0){
+            words[i]='EVEN';
+        }
+    }
+    console.log(words.join(' '));
+}
+replaceeven("I am learning JavaScript every day");
+//task 4
+function remove(input) {
+    let store = '';
+    input = input.split('');
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] !== input[i + 1]) {
+            store += input[i];  
+        }
+    }
+    console.log(result); 
+}
+remove('Hello world');
+
 
 
 
